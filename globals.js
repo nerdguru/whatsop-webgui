3b3fbde3f12c3d2cdf9a89e87f84708eae5d7ab6
Pete Johnson <petecj2@gmail.com>
2019-01-16 22:54:38 -0500

    Added a test flag in globals.js for test/clear buttons plus color formatting

---
+++ globals.js
@@ -0,0 +1,4 @@
+channelName = 'Channel-8nj4xobej';
+pubKey = 'pub-c-ebbb24cb-4381-44df-907b-fd08bc2c515f';
+subKey = 'sub-c-5f4787bc-dd5c-11e8-abf2-1e598b800e69';
+testMode = true;
--- pubnub.js
+++ pubnub.js
@@ -13,6 +13,63 @@ function publishSampleMessage(c, t, d) {
       });
 }
 
+// Function for icon color
+function iconColor(color) {
+  if (color == 'green')
+    return '<div class="timeline__icon timeline--success"></div>';
+
+  if (color == 'blue')
+    return '<div class="timeline__icon timeline--info"></div>';
+
+  if (color == 'yellow')
+    return '<div class="timeline__icon timeline--warning"></div>';
+
+  if (color == 'red')
+    return '<div class="timeline__icon timeline--danger"></div>';
+
+  return '<div class="timeline__icon"></div>';
+}
+
+// Function for title color
+function titleColor(color) {
+  if (color == 'green')
+    return '<div class="text-bold text-success">';
+
+  if (color == 'blue')
+    return '<div class="text-bold text-blue">';
+
+  if (color == 'yellow')
+    return '<div class="text-bold text-warning">';
+
+  if (color == 'red')
+    return '<div class="text-bold text-danger">';
+
+  return '<div class="text-bold">';
+}
+
+// Function for item insertion
+function insertItem(timestamp, title, description, color) {
+  var retVal = '';
+  retVal += '<div class="timeline__item">';
+  retVal += iconColor(color);
+  retVal += '<div class="timeline__time">' + timestamp + '</div>';
+  retVal += '<div class="timeline__content">';
+  retVal += titleColor(color) + title + '</div>';
+  retVal += '<div>' + description + '</div>';
+  retVal += '</div>';
+  retVal += '</div>';
+  return retVal;
+}
+
+// Function for current date/time
+function now() {
+  var n = new Date();
+  var retVal = '';
+  retVal = n.getFullYear() + '-' + (n.getMonth() + 1) + '-' + n.getDate();
+  retVal += ' ' + n.getHours() + ':' + n.getMinutes() + ':' + n.getSeconds();
+  return retVal;
+}
+
 // Set up the channel connection
 pubnub = new PubNub({
     publishKey: pubKey,
@@ -29,6 +86,8 @@ pubnub.addListener({
     message: function (msg) {
         console.log(msg.message.title);
         console.log(msg.message.description);
+        $('#timeline_list').prepend(insertItem(now(),
+                            msg.message.title, msg.message.description, 'red'));
       },
 
     presence: function (presenceEvent) {
@@ -41,7 +100,18 @@ pubnub.subscribe({
     channels: [channelName],
   });
 
-// Map the test button
-$('#test').on('click', function () {
-  publishSampleMessage(channelName, 'oh wait', 'I am back');
-});
+if (testMode) {
+  // Insert the buttons
+  $('#form-area').append('<button id="test" class="btn btn--secondary">test</button>');
+  $('#form-area').append('<button id="clear" class="btn btn--negative">clear</button>');
+
+  // Map the test button
+  $('#test').on('click', function () {
+    publishSampleMessage(channelName, 'oh wait', 'I am back');
+  });
+
+  // Map the clear button
+  $('#clear').on('click', function () {
+    $('#timeline_list').empty();
+  });
+}
